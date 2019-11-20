18df53b83cdd7294cb52ce99eb837174019149dc
pythhh <41883530+pythhh@users.noreply.github.com>
2019-11-20 20:49:46 +0700

    add jsonk

---
+++ ProjectOOP/src/WriterTest.java
@@ -0,0 +1,31 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+import java.io.FileWriter;
+import org.json.simple.JSONObject;
+
+
+public class WriterTest {
+
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("D:\\projectOOP\\ProjectOOP\\src\\file1.txt"));
+            JSONArray array = (JSONArray) obj;
+            JSONObject obj2;
+            for (int i = 0; i < array.size(); i++) {
+                obj2 = (JSONObject) array.get(i);
+                System.out.println(obj2.get("chapter"));
+                System.out.println(obj2.get("set"));
+                System.out.println(obj2.get("word"));
+                System.out.println(obj2.get("meaning"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
