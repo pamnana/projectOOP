10449a1ceac720c74a334cbe2c215948a09ba7da
pythhh <41883530+pythhh@users.noreply.github.com>
2019-11-20 22:24:26 +0700

    add vocab and writertest :cake:

--- ProjectOOP/src/WriterTest.java
+++ ProjectOOP/src/WriterTest.java
@@ -1,31 +1,85 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import org.json.simple.*;
-import org.json.simple.parser.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.io.FileWriter;
-import org.json.simple.JSONObject;
+/**
+ *
+ * @author dell
+ */
+import java.io.*;
+import org.json.simple.*; 
 
+class Vocab {
+    private String chapter;
+    private String set;
+    private String word;
+    private String meaning;
 
+    public Vocab(String chapter, String set, String word, String meaning) {
+        this.chapter = chapter;
+        this.set = set;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public String getSet() {
+        return set;
+    }
+
+    public void setSet(String set) {
+        this.set = set;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+    
+}
 public class WriterTest {
-
     public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
+        Vocab vcb1 = new Vocab("1", "A", "iliterate", "อ่านไม่ออกเขียนไม่ได้");
+        JSONObject obj = new JSONObject();
+        JSONArray array = new JSONArray();
+        obj.put("chapter", vcb1.getChapter());
+        obj.put("set", vcb1.getSet());
+        obj.put("word", vcb1.getWord());
+        obj.put("meaning", vcb1.getMeaning());
+        array.add(obj);
+        System.out.println(array);
+        
         try {
-            Object obj = parser.parse(new FileReader("D:\\projectOOP\\ProjectOOP\\src\\file1.txt"));
-            JSONArray array = (JSONArray) obj;
-            JSONObject obj2;
-            for (int i = 0; i < array.size(); i++) {
-                obj2 = (JSONObject) array.get(i);
-                System.out.println(obj2.get("chapter"));
-                System.out.println(obj2.get("set"));
-                System.out.println(obj2.get("word"));
-                System.out.println(obj2.get("meaning"));
-            }
-        } catch (Exception e) {
+            FileWriter file = new FileWriter("src\\file2.txt");
+            file.write(obj.toJSONString());
+            file.flush();
+            file.close();
+            System.out.println("Success!");
+        }
+        catch (Exception e){
             System.out.println(e);
         }
     }
-
 }
