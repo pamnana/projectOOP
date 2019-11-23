dc81031933baf04e2192d31f6ebabfc496f867a6
pythhh <41883530+pythhh@users.noreply.github.com>
2019-11-23 12:48:31 +0700

    :+1:

--- ProjectOOP/src/Profile.java
+++ ProjectOOP/src/Profile.java
@@ -1,13 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.*;
+import java.util.Random;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
-/**
- *
- * @author DELL
- */
 public class Profile {
-   
+    private String name;
+    private int score;
+
+    public Profile() {
+    }
+
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        try {
+            /*Object obj = parser.parse(new FileReader("src\\file1.txt"));
+            JSONArray array = (JSONArray) obj;
+            JSONObject obj2;
+
+            Random r = new Random();
+            int r_int = r.nextInt(array.size() + 1);
+            obj2 = (JSONObject) array.get(r_int);*/
+
+            //Vocab vcb1 = new Vocab((String) obj2.get("chapter"), (String) obj2.get("word"), (String) obj2.get("meaning"));
+            
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
 }
