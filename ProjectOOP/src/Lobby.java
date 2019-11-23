b5d571f2ef53da56fd1c80ccecbfd5b8f8a0ddca
Wareerat Pinkaraket <32677870+pamnana@users.noreply.github.com>
2019-11-23 11:14:45 +0700

    add gui lobby / result

--- ProjectOOP/src/Lobby.java
+++ ProjectOOP/src/Lobby.java
@@ -26,11 +26,81 @@ public class Lobby extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel9 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(1024, 768));
         setPreferredSize(new java.awt.Dimension(1024, 768));
         setSize(new java.awt.Dimension(1024, 768));
         getContentPane().setLayout(null);
+
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
+        jButton1.setFont(new java.awt.Font("Superspace Bold", 0, 24)); // NOI18N
+        jButton1.setText("Ready!");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(430, 550, 170, 51);
+
+        jPanel1.setFocusTraversalPolicyProvider(true);
+        jPanel1.setOpaque(false);
+        jPanel1.setLayout(new java.awt.GridLayout(2, 0));
+
+        jPanel2.setBackground(new java.awt.Color(102, 0, 0));
+        jPanel2.setLayout(new java.awt.GridLayout());
+
+        jLabel9.setBackground(new java.awt.Color(153, 0, 0));
+        jLabel9.setFont(new java.awt.Font("Superspace Bold", 0, 72)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("Player Battle");
+        jLabel9.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel2.add(jLabel9);
+
+        jPanel1.add(jPanel2);
+
+        jPanel3.setOpaque(false);
+        jPanel3.setLayout(new java.awt.GridLayout(2, 0));
+
+        jLabel5.setFont(new java.awt.Font("RSU", 0, 36)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Player 1");
+        jPanel3.add(jLabel5);
+
+        jLabel7.setFont(new java.awt.Font("RSU", 0, 36)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel7.setText("Waiting...");
+        jPanel3.add(jLabel7);
+
+        jLabel6.setFont(new java.awt.Font("RSU", 0, 36)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Player 2");
+        jPanel3.add(jLabel6);
+
+        jLabel8.setFont(new java.awt.Font("RSU", 0, 36)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("Waiting...");
+        jPanel3.add(jLabel8);
+
+        jPanel1.add(jPanel3);
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(180, 240, 670, 280);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vs.jpg"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 1024, 768);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -71,5 +141,15 @@ public class Lobby extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
