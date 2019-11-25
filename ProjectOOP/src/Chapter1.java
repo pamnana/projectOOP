c7c1cf4f0c3f789620d9c46ba5e9210acacb40c2
Wareerat Pinkaraket <32677870+pamnana@users.noreply.github.com>
2019-11-25 12:49:50 +0700

    up size JFrame

--- ProjectOOP/src/Chapter1.java
+++ ProjectOOP/src/Chapter1.java
@@ -191,6 +191,7 @@ public class Chapter1 extends javax.swing.JFrame {
         jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1024, 768));
         setResizable(false);
         setSize(new java.awt.Dimension(1024, 768));
         addComponentListener(new java.awt.event.ComponentAdapter() {
